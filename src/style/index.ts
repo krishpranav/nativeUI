e4eb6a57fba47523161e2fbc0d99f63494a0c71b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-12 16:00:14 +0530

    style: index[callback]

---
+++ src/style/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @file index.ts
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-05-12
+ *
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ *
+ */
+
+export { defaultTheme } from './default';
